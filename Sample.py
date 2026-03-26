2fc00dae4f0e4517232fbc61d89d40ae88dff060
dhanushlakshmipathy <lakshmipathydhanush@gmail.com>
2026-03-26 16:57:51 +0530

    Adding sample

---
+++ Sample.py
@@ -0,0 +1,9 @@
+import os
+
+def connect_to_rds():
+    # TODO: Junior dev note - remove this before pushing to production!
+    db_admin_password = "AKIA_SUPER_SECRET_KEY_999"
+    
+    # Normal logic
+    print("Connecting to the database...")
+    return True
